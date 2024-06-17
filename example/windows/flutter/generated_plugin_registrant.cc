e06eda8375fb50ca92c725521ec81d65d50d8acb
ji <237525603@qq.com>
2024-06-17 10:17:37 +0800

    add: windows support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <screen_pixel/screen_pixel_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ScreenPixelPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenPixelPlugin"));
+}
